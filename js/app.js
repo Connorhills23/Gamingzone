5e6fbcb439a6503f42a8c3a6294c727c26724453
Connor Hills <73914645+Connorhills23@users.noreply.github.com>
2026-01-08 20:20:31 +0000

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,68 @@
+// Date Code
+var output = document.querySelector("#output");
+
+var date = new Date();
+
+var day = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+
+if (day < 10) {
+  day = "0" + day;
+}
+
+if (month < 10) {
+  month = "0" + month;
+}
+
+output.innerHTML = `&#127918; ${day} / ${month} / ${year} &#127918; `;
+
+// Clock Code
+function showTime() {
+  const date = new Date();
+  let h = date.getHours(); // 0 - 23
+  let m = date.getMinutes(); // 0 - 59
+  let s = date.getSeconds(); // 0 - 59
+  let session = "AM";
+
+  if (h == 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  var time = h + ":" + m + ":" + s + " " + session;
+  document.getElementById("MyClockDisplay").innerText = time;
+  document.getElementById("MyClockDisplay").textContent = time;
+
+  setTimeout(showTime, 1000);
+}
+
+showTime();
+
+// Back to Top Button Code
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
